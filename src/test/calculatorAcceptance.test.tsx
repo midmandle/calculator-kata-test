403250777d76d056a082a93c5552914cb17ba536
Moises Rodriguez <moisesrj97@gmail.com>
2023-06-28 14:17:18 +0200

    add substraction acceptance test

--- src/test/calculatorAcceptance.test.tsx
+++ src/test/calculatorAcceptance.test.tsx
@@ -26,4 +26,26 @@ describe('Calculator Acceptance', () => {
 
         expect(outputText).toHaveTextContent('201');
     });
+    test('user can substract two numbers together and see the result', async () => {
+        render(<Calculator/>)
+
+        const number1Button = screen.getByRole('button', {name: '1'});
+        const number9Button = screen.getByRole('button', {name: '9'});
+        const minusOperatorButton = screen.getByRole('button', {name: '-'});
+        const number8Button = screen.getByRole('button', {name: '8'});
+        const number2Button = screen.getByRole('button', {name: '2'});
+        const equalsButton = screen.getByRole('button', {name: '='});
+
+        await userEvent.click(number1Button);
+        await userEvent.click(number9Button);
+        await userEvent.click(number2Button);
+        await userEvent.click(minusOperatorButton);
+        await userEvent.click(number1Button);
+        await userEvent.click(number8Button);
+        await userEvent.click(equalsButton);
+
+        const outputText = screen.getByRole('textbox');
+
+        expect(outputText).toHaveTextContent('174');
+    });
 });
