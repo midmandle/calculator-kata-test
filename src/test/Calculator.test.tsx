8b57ddf5fa437971e7075f71cdeccfb25f8a70ab
K Leonardo Ruiz Flórez <kleonardoruizflorez@gmail.com>
2023-06-28 10:57:07 +0200

    pascal case component names

--- src/test/Calculator.test.tsx
+++ src/test/Calculator.test.tsx
@@ -1,4 +1,4 @@
-import {Calculator} from "../calculator";
+import {Calculator} from "../Calculator.tsx";
 import {render, screen} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
--- src/test/calculatorAcceptance.test.tsx
+++ src/test/calculatorAcceptance.test.tsx
@@ -1,6 +1,6 @@
 import {describe, test} from "vitest";
 import {render, screen} from "@testing-library/react";
-import {Calculator} from "../calculator";
+import {Calculator} from "../Calculator.tsx";
 import userEvent from "@testing-library/user-event";
 
 describe('Calculator Acceptance', () => {
