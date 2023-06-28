d9be8dd0aee42d53b3f4256aa248d739112ebc6d
Alasdair Smith <alasdairsmith0491@gmail.com>
2023-06-28 11:54:30 +0100

    Update acceptance to have more complete addition test

--- src/test/calculatorAcceptance.test.tsx
+++ src/test/calculatorAcceptance.test.tsx
@@ -7,18 +7,23 @@ describe('Calculator Acceptance', () => {
     test('user can add two numbers together and see the result', async () => {
         render(<Calculator/>)
 
+        const number1Button = screen.getByRole('button', {name: '1'});
         const number9Button = screen.getByRole('button', {name: '9'});
         const plusOperatorButton = screen.getByRole('button', {name: '+'});
         const number8Button = screen.getByRole('button', {name: '8'});
+        const number2Button = screen.getByRole('button', {name: '2'});
         const equalsButton = screen.getByRole('button', {name: '='});
 
+        await userEvent.click(number1Button);
         await userEvent.click(number9Button);
         await userEvent.click(plusOperatorButton);
+        await userEvent.click(number1Button);
         await userEvent.click(number8Button);
+        await userEvent.click(number2Button);
         await userEvent.click(equalsButton);
 
         const outputText = screen.getByRole('textbox');
 
-        expect(outputText).toHaveTextContent('17');
+        expect(outputText).toHaveTextContent('201');
     });
 });
